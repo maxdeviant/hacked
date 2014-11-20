f057681670f6883411a426a0f9be02d165a18830
Marshall Bowers <elliott.codes@gmail.com>
2014-11-20 16:10:35 -0500

    Add basic command execution

--- hacked.js
+++ hacked.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var path = require('path');
 var debug = require('debug')('express');
@@ -12,6 +13,9 @@ var io = require('socket.io')(server);
 var models = require('./models');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,6 +24,15 @@ var router = express.Router();
 router.route('/')
     .get(function (req, res) {
         return res.render('index');
+    });
+
+router.route('/execute')
+    .post(function (req, res) {
+        var command = req.body.command;
+
+        return res.status(200).json({
+            message: 'Command: "' + command + '" received.'
+        });
     });
 
 app.use('/', router);
