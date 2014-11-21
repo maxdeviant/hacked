1fd7a7007e0ddd561d2a2b28cde666e7258e4696
Marshall Bowers <elliott.codes@gmail.com>
2014-11-20 20:03:25 -0500

    Fix function spacing

--- models/index.js
+++ models/index.js
@@ -10,15 +10,15 @@ var db = {};
 
 fs
     .readdirSync(__dirname)
-    .filter(function(file) {
+    .filter(function (file) {
         return (file.indexOf('.') !== 0) && (file !== 'index.js');
     })
-    .forEach(function(file) {
+    .forEach(function (file) {
         var model = sequelize['import'](path.join(__dirname, file));
         db[model.name] = model;
     });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(function (modelName) {
     if ('associate' in db[modelName]) {
         db[modelName].associate(db);
     }
