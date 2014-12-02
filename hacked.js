8f7d2f2c78a85daea7d2e21a89aed36dd15b39ca
Marshall Bowers <elliott.codes@gmail.com>
2014-12-02 11:48:47 -0500

    Change server startup message

--- hacked.js
+++ hacked.js
@@ -143,5 +143,5 @@ app.use('/', router);
 app.set('port', process.env.PORT || 3000);
 
 server.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
+    console.log('Hacked server listening on port ' + server.address().port);
 });
